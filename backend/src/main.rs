2b891162dbac591e41d537621be8507cfee52ef3
Erkki Seppälä <flux@inside.org>
2025-06-14 13:34:02 +0300

    main: reduced default log level to error

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,6 +7,6 @@ use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    env_logger::init_from_env(Env::default().default_filter_or("error"));
     crate::server::main().await
 }
