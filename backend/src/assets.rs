11e26e20771be610365b3f74d74b9bd1752120b0
Erkki Seppälä <flux@inside.org>
2025-06-22 18:37:43 +0300

    backend: fixed missing content type for embedded assets

--- backend/src/assets.rs
+++ backend/src/assets.rs
@@ -1,10 +1,13 @@
 use actix_web::{web, Scope};
 
 #[cfg(feature = "embed-assets")]
-use actix_web_rust_embed_responder::{EmbedResponse, IntoResponse};
+use mime_guess::from_path;
 
 #[cfg(feature = "embed-assets")]
-use rust_embed::{EmbeddedFile, RustEmbed};
+use actix_web::{HttpResponse, Responder};
+
+#[cfg(feature = "embed-assets")]
+use rust_embed::RustEmbed;
 
 #[cfg(feature = "embed-assets")]
 #[derive(RustEmbed)]
@@ -15,13 +18,22 @@ struct EmbeddedAssets;
 async fn embedded_assets_handler(
     path: web::Path<String>,
     index_file_name_data: web::Data<String>,
-) -> EmbedResponse<EmbeddedFile> {
+) -> impl Responder {
     let file_path = if path.is_empty() {
         index_file_name_data.as_str()
     } else {
         path.as_str()
     };
-    EmbeddedAssets::get(file_path).into_response()
+
+    if let Some(embedded_file) = EmbeddedAssets::get(file_path) {
+        let mime_type = from_path(file_path).first_or_octet_stream(); // Fallback to application/octet-stream
+
+        HttpResponse::Ok()
+            .content_type(mime_type.to_string())
+            .body(embedded_file.data)
+    } else {
+        HttpResponse::NotFound().body("File not found")
+    }
 }
 
 pub fn assets(path_prefix: &str, index_file_name: &str) -> Scope {
